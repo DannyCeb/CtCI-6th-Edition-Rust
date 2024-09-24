b290b6ea26ae3521b9115837186d39cf69802542
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2024-09-24 15:50:39 -0600

    Add Error Trait for likedlisterror

--- Ch_02_Linked_lists/linked_list_problems/src/linked_list/ll_error.rs
+++ Ch_02_Linked_lists/linked_list_problems/src/linked_list/ll_error.rs
@@ -1,8 +1,22 @@
+use std::error::Error;
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum LinkedListError {
     NotFound,
     EmptyList,
 }
+
+impl Display for LinkedListError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LinkedListError::EmptyList => write!(f, "The linked list is empty"),
+            LinkedListError::NotFound => write!(f, "Item/Node not found"),
+        }
+    }
+}
+
+impl Error for LinkedListError {}
 
 impl PartialEq for LinkedListError {
     fn eq(&self, other: &Self) -> bool {
