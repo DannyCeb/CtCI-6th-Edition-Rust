e5846070d7ac042e96a78a68d60140279d413876
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2024-09-26 15:05:38 -0600

    q3_01_three_in_one

--- Ch_03_stacks_and_queues/stacks_and_queues_problems/src/lib.rs
+++ Ch_03_stacks_and_queues/stacks_and_queues_problems/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod structs;
 
+pub mod q3_01_three_in_one;
+
 pub trait NodeItemTraits: Copy + std::fmt::Display + Default + Clone {}
 impl NodeItemTraits for i32 {}
 impl NodeItemTraits for char {}
