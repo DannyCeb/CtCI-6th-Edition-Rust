d11862801611c040aa46788ff88c607966fba331
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2024-09-28 12:04:33 -0600

    q3_04_queue_via_stacks

--- Ch_03_stacks_and_queues/stacks_and_queues_problems/src/lib.rs
+++ Ch_03_stacks_and_queues/stacks_and_queues_problems/src/lib.rs
@@ -3,6 +3,7 @@ pub mod structs;
 pub mod q3_01_three_in_one;
 pub mod q3_02_stack_min;
 pub mod q3_03_stack_of_plates;
+pub mod q3_04_queue_via_stacks;
 
 pub trait NodeItemTraits: Copy + std::fmt::Display + Default + Clone {}
 impl NodeItemTraits for i32 {}
